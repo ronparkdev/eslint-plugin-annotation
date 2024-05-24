b2fe61c7040d6076f716d6fed52f5501b44baf70
ronparkdev <ronpark.dev@gmail.com>
2024-05-25 01:44:56 +0900

    fix: Add format-date rule into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import formatDate from './rules/format-date'
 import sort from './rules/sort'
 import sortKeys from './rules/sort-keys'
 
 // Import all rules in lib/rules
 module.exports.rules = {
+  'format-date': formatDate,
   sort: sort,
   'sort-keys': sortKeys,
 }
