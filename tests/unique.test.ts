21f19caf002a29b9df9bbb7963f39377018bfa04
ronparkdev <ronpark.dev@gmail.com>
2024-05-29 19:52:30 +0900

    test: Add test code for unique annotation

---
+++ tests/unique.test.ts
@@ -0,0 +1,64 @@
+import path from 'path'
+
+import { AST_NODE_TYPES, ESLintUtils } from '@typescript-eslint/utils'
+
+const { RuleTester } = ESLintUtils
+
+import rule from '../src/rules/unique'
+
+const getFilename = (filePath: string): string => path.resolve('./tests', filePath)
+
+const ruleTester = new RuleTester({
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2015,
+    project: getFilename('tsconfig.json'),
+    sourceType: 'module',
+  },
+})
+
+ruleTester.run('unique', rule, {
+  valid: [
+    {
+      code: `
+      // @unique
+      const ids = [1, 2, 3, 4, 5];
+      `,
+      filename: getFilename('main.ts'),
+    },
+    {
+      code: `
+      // @unique
+      const names = ["Alice", "Bob", "Charlie"];
+      `,
+      filename: getFilename('main.ts'),
+    },
+  ],
+  invalid: [
+    {
+      code: `
+      // @unique
+      const ids = [1, 2, 2, 4, 5];
+      `,
+      errors: [{ messageId: 'duplicateValues', type: AST_NODE_TYPES.ArrayExpression }],
+      output: `
+      // @unique
+      const ids = [1, 2, 4, 5];
+      `,
+      filename: getFilename('main.ts'),
+    },
+    {
+      code: `
+      // @unique
+      const names = ["Alice", "Bob", "Alice"];
+      `,
+      errors: [{ messageId: 'duplicateValues', type: AST_NODE_TYPES.ArrayExpression }],
+      output: `
+      // @unique
+      const names = ["Alice", "Bob"];
+      `,
+      filename: getFilename('main.ts'),
+    },
+  ],
+})
